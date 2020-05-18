4b5d57e8374515d0a14d68eb5682592578443b6a
Henk Willcock <henkwillcock@catalyst.net.nz>
2020-05-18 23:35:39 +1200

    Initial commit

---
+++ FirstProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    public float smoothSpeed = 0.125f;
+    public float lookDistance = 10;
+    public Vector3 zOffset = new Vector3(0, 0, -20);
+
+    // Update is called once per frame
+    void LateUpdate()
+    {   
+        Vector3 mousePosition = Input.mousePosition - new Vector3(Screen.width/2, Screen.height/2, 0);
+        Vector3 desiredPosition = target.position + zOffset + mousePosition/Screen.width*lookDistance;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+    }
+}
---
+++ FirstProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0.02f;
+
+    void Update()
+    {
+        transform.position += CalculateVelocity();
+    }
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("We hit something");
+    }
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("We toasd something");
+    }
+
+    public Vector3 CalculateVelocity()
+    {
+        float leftMovement = System.Convert.ToSingle(Input.GetKey("a"));
+	    float rightMovement = System.Convert.ToSingle(Input.GetKey("d"));
+        float horizontalMovement = (rightMovement - leftMovement);
+
+	    float upMovement = System.Convert.ToSingle(Input.GetKey("w"));
+	    float downMovement = System.Convert.ToSingle(Input.GetKey("s"));
+        float verticalMovement = (upMovement - downMovement);
+
+	    Vector3 velocity = new Vector3(horizontalMovement, verticalMovement, 0) * moveSpeed;
+
+        if (horizontalMovement != 0 && verticalMovement != 0) {
+            // So the player doesn't move faster when travelling diagonally.
+            velocity /= System.Convert.ToSingle(System.Math.Sqrt(2));
+        }
+
+        return velocity;
+    }
+
+
+}
