d1d8aca2e4123666b8b17d9d5e1ab195561edf0e
Henk Willcock <henkwillcock@catalyst.net.nz>
2020-05-27 14:22:27 +1200

    ASDASDSDA

--- FirstProject/Assets/Scripts/Player/PlayerMovement.cs
+++ FirstProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private enum Weapon {
+        Spin,
+        Pistol
+    }
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float spinSpeed;
+    [SerializeField] private Weapon weapon;
+
     public Rigidbody rigidbody;
+    public Rigidbody bulletPrefab;
 
     void Start() {
         rigidbody.maxAngularVelocity = spinSpeed;
@@ -16,7 +24,18 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButton(0)) {
-            rigidbody.angularVelocity = new Vector3(0, 0, spinSpeed);
+            if (weapon == Weapon.Spin) {
+                rigidbody.angularVelocity = new Vector3(0, 0, spinSpeed);
+            } else if (weapon == Weapon.Pistol) {
+                rigidbody.angularVelocity = Vector3.zero;
+                var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(rigidbody.position);
+                var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90;
+                rigidbody.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+                Rigidbody bullet;
+                bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+                bullet.velocity = transform.TransformDirection(Vector3.forward * 10);
+            }
         } else {
             rigidbody.angularVelocity = Vector3.zero;
             var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(rigidbody.position);
