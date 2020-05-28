90564a8d748afe90049df95d42b44b53c1a36ed0
Henk Willcock <henkwillcock@catalyst.net.nz>
2020-05-29 09:24:52 +1200

    ASD

---
+++ FirstProject/Assets/Scripts/CarDriving.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDriving : MonoBehaviour
+{
+    private GameController gameController;
+
+    public Rigidbody rigidbody;
+    public Transform transform;
+
+    public float thrust;
+    public float steeringPower;
+    public bool beingDriven = false;
+
+    void Start() {
+        this.gameController = GameObject.FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameController.gameFocus == this.rigidbody) {
+
+            // Forward/Backward
+            if (Input.GetKey("s")) {
+                rigidbody.AddForce(-this.transform.right * thrust);
+            } else if (Input.GetKey("w")) {
+                // Accelerate
+                rigidbody.AddForce(this.transform.right * thrust);
+            }
+
+            // Steering
+            float steerLeft = System.Convert.ToSingle(Input.GetKey("a"));
+            float steerRight = System.Convert.ToSingle(Input.GetKey("d"));
+            float steering = steerLeft - steerRight;
+            rigidbody.AddTorque(transform.forward * steering * steeringPower);
+
+            // Exit Vehicle
+            if (Input.GetKeyDown("f")) {
+                gameController.player.gameObject.SetActive(true);
+                gameController.player.position = this.rigidbody.position + transform.up * -3;
+                gameController.gameFocus = gameController.player;
+            }
+        }
+    }
+}
---
+++ FirstProject/Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameController gameController;
+    public Transform transform;
+
+    public void Start() {
+        this.gameController = GameObject.FindObjectOfType<GameController>();
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Vehicle" && Input.GetKeyDown("e")) {
+            this.gameController.gameFocus = other.attachedRigidbody;
+            this.gameController.cameraType = "Following";
+            transform.gameObject.SetActive(false);
+        }
+    }
+}
